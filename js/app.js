7ac119a08409846fa0670fafa252fcc4d554b0c2
Roslyn Saplicki <j.saplicki@gmail.com>
2018-09-27 16:34:56 -0700

    The full JS and logic implementation.

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@
 
 const SUITS = ["D", "C", "H", "S"];
 const RANKS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-const CARDBACK = `path`
 
 /*----- app's state (variables) -----*/
 
@@ -14,7 +13,7 @@ var stock;
 var waste;
 var tableauSections;
 var selectedCards;
-var cardsToTurn;
+
 
 var staticColIdx; 
 var staticRowIdx; 
@@ -28,10 +27,11 @@ var stockEl = document.getElementById("stock");
 var tableauEl = document.getElementById('tableau');
 var resetBtn = document.querySelector('button');
 var foundationEl = document.getElementById('foundation');
-var pumpkinBoard = document.getElementById('woodstock');
+var woodstock = document.querySelector('.woodstock');
 
 /*----- event listeners -----*/
 
+wasteEl.addEventListener("click", selectWaste);
 foundationEl.addEventListener("click", buildFoundation);
 tableauEl.addEventListener("click", handleColumnClick);
 stockEl.addEventListener("click", addWaste);
@@ -43,40 +43,62 @@ function init() {
     deck = [];
     activeCard = []; // this is where the active card in play is stored
     stock = [];
+    waste = [];
     tableau = [[], [], [], [], [], [], []];
     foundation = [[], [], [], []];
     selectedCards = [];
     createDeck(); 
     shuffleDeck();
     deal(); 
-    cardsToTurn = stock.length -1;
+    waste = waste.map(function(card) {
+        card.isActive = true;
+        return card;
+    });
     render();
     renderStockWaste(true); 
     // displayActive(); // show active cards function?
 }
 
 function addWaste() {
-    cardsToTurn--;
-    stock.push(stock.shift());
-    renderStockWaste();
-    if (!cardsToTurn) cardsToTurn = stock.length -1;
+    if (waste.length) {
+        stock.unshift(waste.pop()); 
+    } else {
+        waste = stock;
+        stock = [];
+    }
+    renderStockWaste(); 
 }
 
 function handleColumnClick() {
     var colIdx = parseInt(event.target.id.charAt(1));
-    var rowIdx = parseInt(event.target.id.charAt(3));
+    var rowIdx = parseInt(event.target.id.substr(3));
     
     if (selectedCards.length) {
-        tableau[colIdx] = tableau[colIdx].concat(selectedCards);
-        selectedCards = [];
-        tableau[staticColIdx].splice(staticRowIdx);
-        if (tableau[colIdx][rowIdx].isActive) {
-            tableau[colIdx].forEach(function(card){
-                card.selected = false;
-            });
+        // check if selectedCards contains the stock[0]
+        if (selectedCards[0] === stock[0]) {
+            if (validTableauMove(colIdx)) {
+                tableau[colIdx] = tableau[colIdx].concat(selectedCards);
+                selectedCards = [];
+                stock.splice(0, 1);
+                if (stock.length) stock.unshift(stock.pop());
+            }
+            renderStockWaste();
+        } else {
+            // move previously selected cards
+            if (validTableauMove(colIdx)) {
+                tableau[colIdx] = tableau[colIdx].concat(selectedCards);
+                selectedCards = [];
+                tableau[staticColIdx].splice(staticRowIdx);
+                if (tableau[staticColIdx].length) {
+                    var lastCardIdx = tableau[staticColIdx].length - 1;
+                    tableau[staticColIdx][lastCardIdx].isActive = true;
+                }
+            } else {
+                selectedCards = [];
+            }
         }
     } else {
-    if(!tableau[colIdx][rowIdx].isActive) return; // AND NO OTHER BELOW IT ... click to make it active?
+        if(!tableau[colIdx][rowIdx].isActive) return; // AND NO OTHER BELOW IT ... click to make it active?
         staticColIdx = parseInt(event.target.id.charAt(1));
         staticRowIdx = parseInt(event.target.id.charAt(3));
         selectedCards = tableau[colIdx].slice(rowIdx);
@@ -86,18 +108,37 @@ function handleColumnClick() {
     render();
 }
 
-// add flipping of the next card in the column array if accepted into the other array(static column row -1 make is active... show image link)
-// dont let it click on it's own column (may be a logic addition)
+// add flipping of the next card in the column array if accepted into the other array(static column row -1 make is active, if not already... show image link)
+// dont let it click on it's own column (may be a logic addition) then it dissapears which is an issue
 
 // selectWaste that can then be moved into the selectedCards Array
-// stock[0]=stock[0].shift(tableau[colIdx].pop());
+
+function selectWaste() {
+    if(selectedCards.length) {
+        selectedCards = [];
+    } else {
+        selectedCards = [stock[0]]; //because zero is always the first
+    }
+    renderStockWaste();
+}
 
 function buildFoundation() {
     if (selectedCards.length === 1) {
-    var correctFoundation = event.target.id.charAt(1)
-    foundation[correctFoundation].push(selectedCards.pop());
-    tableau[staticColIdx].splice(staticRowIdx);
-    render();
+        var correctFoundation = event.target.id.charAt(1)
+        if (validFoundationMove(correctFoundation)) {
+            if (selectedCards[0] === stock[0]) {
+                stock.splice(0, 1);
+                if (stock.length) stock.unshift(stock.pop());
+                renderStockWaste();
+            } else {
+                tableau[staticColIdx].splice(staticRowIdx);
+                var lastCardIdx = tableau[staticColIdx].length - 1;
+                if (lastCardIdx > -1) tableau[staticColIdx][lastCardIdx].isActive = true;
+            }
+            foundation[correctFoundation].push(selectedCards.pop());
+            selectedCards = [];
+            render();
+        }
     }
 }
 
@@ -105,7 +146,7 @@ function render() {
     tableauCols.forEach(function(section, tableauColIdx) {
         var html = '';
         tableau[tableauColIdx].forEach(function(card, rowIdx) {
-            html += `<div ${card.selected ? 'class="selected"' : "" }><img id="c${tableauColIdx}r${rowIdx}" src="${card.isActive ? card.imgLink : 'img/BLUEBACK.png'}"></div>`;
+            html += `<div ${selectedCards.includes(card) ? 'class="selected"' : "" }><img id="c${tableauColIdx}r${rowIdx}" src="${card.isActive ? card.imgLink : 'img/BLUEBACK.png'}"></div>`;
         });
         section.innerHTML = html;
     });
@@ -116,15 +157,23 @@ function render() {
             foundationPiles[i].setAttribute("style", `background-image: url('${foundation[i][foundation[i].length - 1].imgLink}');`);
         } 
     })
+    renderStockWaste();
     checkWin();
 }
+
 function renderStockWaste(firstClick){
-    if (cardsToTurn) {
+    if (waste.length) {
         stockEl.setAttribute("style","background-image:url('img/BLUEBACK.png');");
-        if (!firstClick) wasteEl.setAttribute("style",`background-image:url('${stock[0].imgLink}');`)   
-    } else {
+    } else if (stock.length) {
         stockEl.setAttribute("style","background-image:url('img/refresh.png');");
-        wasteEl.setAttribute("style", "background-image: none;");
+    } else {
+        stockEl.setAttribute("style", "visibility: hidden;");
+    }
+    if (stock.length) {
+        wasteEl.setAttribute("style",`background-image:url('${stock[0].imgLink}');`);
+        selectedCards.includes(stock[0]) ? wasteEl.setAttribute("class","selected") : wasteEl.removeAttribute("class");
+    } else {
+        wasteEl.setAttribute("style", "visibility: hidden;");
     }
 } 
 
@@ -137,19 +186,20 @@ class Card {
         this.imgLink = (`img/${this.suit}${this.rank}.png`)
     }
 }
+
 function shuffleDeck() {
     for (var i = 0; i < 52; i++) {
       var randShuffle = Math.floor(Math.random() * deck.length);
-      stock.push(deck[randShuffle]);
+      waste.push(deck[randShuffle]);
       deck.splice(randShuffle, 1);
     }
-    return stock;
+    return waste;
 }
 
 function deal() {
     tableau.forEach(function(colArr,colIdx){ 
         for(var i = 0; i <= colIdx; i++) {
-            colArr.push(stock.pop());
+            colArr.push(waste.pop());
             if (i === colIdx) colArr[i].isActive = true;
         } 
     }); 
@@ -163,17 +213,49 @@ function createDeck () {
     }
 }
 
+function validTableauMove(targetColIdx) {
+    if(!tableau[targetColIdx].length && selectedCards[0].rank === 13) return true;
+    // obtain first card in selectedCards & last Card in the target column
+    return altColorCheck(selectedCards[0], tableau[targetColIdx][tableau[targetColIdx].length - 1]) &&
+        descOrderCheck(selectedCards[0], tableau[targetColIdx][tableau[targetColIdx].length - 1]);
+}
+
+function validFoundationMove(correctFoundation) {
+    var f = foundation[correctFoundation];
+    if (!f.length && selectedCards[0].rank === 1) return true;
+    return sameColorCheck(selectedCards[0], f[f.length - 1]) &&
+        ascOrderCheck(selectedCards[0], f[f.length - 1]);
+}
+
+function altColorCheck(card1, card2) {
+    var card1S = SUITS.indexOf(card1.suit);
+    var card2S = SUITS.indexOf(card2.suit);
+    return card1S % 2 !== card2S % 2;
+}
+
+function sameColorCheck(card1, card2) {
+    var card1S = SUITS.indexOf(card1.suit);
+    var card2S = SUITS.indexOf(card2.suit);
+    return card1S % 2 === card2S % 2;
+}
+
+function ascOrderCheck(card1, card2) {
+    return card1.rank === card2.rank + 1;
+}
+
+function descOrderCheck(card1, card2) {
+    return card1.rank === card2.rank - 1;
+}
+
 function checkWin() {
-    if (foundation[0].length + foundation[1].length + foundation[2].length + foundation[3].length === 52){
-        woodstock.setAttribute("style", "background-image: none;") // this is where I will insert the visibility function ! 
+    if ((foundation[0].length + foundation[1].length + foundation[2].length + foundation[3].length) === 52){
+        woodstock.textContent = "YOU WON!" 
     };
 }
 
-
-// columns should be constructors and loading decks are in arrays
-
 function reset() {
     init();
+    wasteEl.setAttribute("style", "background-image: none;");
 }
 
 init();
